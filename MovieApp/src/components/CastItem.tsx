a53ead721f2d86f3072f80dc8224c7bb75c8babe
marcelo.sanabria <pucky_one@hotmail.com>
2021-12-04 00:27:17 -0400

    166 - lista de actores

--- MovieApp/src/components/CastItem.tsx
+++ MovieApp/src/components/CastItem.tsx
@@ -12,11 +12,15 @@ export const CastItem = ({actor}: Props ) => {
     const uri = `https://image.tmdb.org/t/p/w500${actor.profile_path}`;
     return (
         <View style={styles.container}>
-
-            <Image
-                source={{ uri }}
-                style={{width: 50, height: 50, borderRadius: 10}}
-            />
+            {
+                actor.profile_path && (
+                    <Image
+                        source={{ uri }}
+                        style={{width: 50, height: 50, borderRadius: 10}}
+                    />
+                )
+            }
+            
             <View style={styles.actorinfo }>
                 <Text style={{fontSize: 18, fontWeight:'bold'}}>
                     {actor.name}
@@ -36,6 +40,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         backgroundColor: 'white',
         borderRadius: 10,
+        height: 50,
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -45,8 +50,14 @@ const styles = StyleSheet.create({
         shadowRadius: 7,
 
         elevation: 10,
+        
+        marginHorizontal: 20,
+        marginLeft: 20,
+        paddingRight: 15,
+        
     },
     actorinfo: {
-        marginLeft: 10
+        marginLeft: 10,
+        marginTop: 4,
     }
 })
--- MovieApp/src/components/MovieDetails.tsx
+++ MovieApp/src/components/MovieDetails.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
 import { Cast } from '../interfaces/creditsInterface'
 import { MovieFull } from '../interfaces/movieInterface'
@@ -45,7 +45,15 @@ export const MovieDetails = ({movieFull, cast}: Props) => {
                 <Text style= {{fontSize: 23, marginTop: 10, fontWeight: 'bold', marginHorizontal: 20}}>
                     Actores
                 </Text>
-                <CastItem actor={cast[0]} />
+                <FlatList
+                    data = {cast}
+                    keyExtractor = { (item)=> item.id.toString()}
+                    renderItem = { ({ item })=> <CastItem actor={item} />}
+                    horizontal = { true }
+                    showsHorizontalScrollIndicator = { false }
+                    style={{marginTop: 10, height: 70}}
+                />
+                {/* <CastItem actor={cast[0]} /> */}
             </View>
         </>
     )
